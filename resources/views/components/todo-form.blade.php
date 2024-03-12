be517bfaf6a5e86fadb89b6915f7b4250d0ed2ab
Ion Caliman <icaliman92@gmail.com>
2024-03-12 11:41:31 +0200

    readme

--- resources/views/components/todo-form.blade.php
+++ resources/views/components/todo-form.blade.php
@@ -1,17 +1,15 @@
 <div id="form" class="mt-16 flex flex-col gap-4 justify-center" hx-swap-oob="true">
-    <form hx-post="/store" hx-target="#todos-list" hx-swap="afterbegin" class="join" {{-- hx-on::after-request=" if(event.detail.successful) this.reset()" --}}>
-        <div class="grow">
-            <input id="title" autofocus name="title" type="text" placeholder="Todo..."
-                value="{{ $title ?? '' }}" @class([
-                    'input input-bordered join-item w-full lg:w-96',
-                    'input-error' => $errors->has('title'),
-                ])>
-            @if ($errors->has('title'))
-                <div class="text-error mt-2">
-                    {{ $errors->first('title') }}
-                </div>
-            @endif
-        </div>
-        <button type="submit" class="btn bg-white join-item">Submit</button>
+    <form hx-post="/store" hx-target="#todos-list" hx-swap="afterbegin" class="relative" {{-- hx-on::after-request=" if(event.detail.successful) this.reset()" --}}>
+        <input id="title" autofocus name="title" type="text" placeholder="What needs to be done?"
+            value="{{ $title ?? '' }}" @class([
+                'input input-bordered w-full lg:w-96 pr-24',
+                'input-error' => $errors->has('title'),
+            ])>
+        @if ($errors->has('title'))
+            <div class="text-error mt-2">
+                {{ $errors->first('title') }}
+            </div>
+        @endif
+        <button type="submit" class="btn btn-sm bg-white top-2 absolute right-2">Submit</button>
     </form>
 </div>
--- resources/views/components/todo.blade.php
+++ resources/views/components/todo.blade.php
@@ -1,10 +1,16 @@
-<a href="#" @class([
+<a id="todo-item-{{ $id }}" href="#" @class([
     'todo-item scale-100 px-6 py-4 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500',
     'opacity-50 line-through' => $done,
     'transition-all h-0 py-0' => $deleted,
-]) hx-post="/toggle/{{ $id }}" hx-target="this" hx-swap="outerHTML">
+]) hx-post="/toggle/{{ $id }}"
+    hx-target="this" hx-swap="outerHTML">
     <div class="w-full flex justify-between">
-        <h2 class="text-xl font-semibold text-gray-900 dark:text-white">{{ $title }}</h2>
+        <h2 class="flex items-center gap-2 text-xl font-semibold text-gray-900 dark:text-white">
+            <input type="checkbox" @if ($done) checked="checked" @endif
+                class="checkbox checkbox-lg checkbox-success border-base-300 rounded-full p-2" />
+
+            {{ $title }}
+        </h2>
         <button class="btn btn-xs btn-ghost btn-circle" hx-delete="/delete/{{ $id }}"
             hx-swap="outerHTML swap:500ms" hx-trigger="click consume">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
